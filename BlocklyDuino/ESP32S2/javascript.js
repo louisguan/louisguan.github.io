2ce8dece32f6c9b166de0cd03ec4f112bc22d215
louisguan <bat869cube672@gmail.com>
2023-03-05 01:20:21 +0800

    Add files via upload

--- BlocklyDuino/ESP32S2/javascript.js
+++ BlocklyDuino/ESP32S2/javascript.js
@@ -1,5 +1,23 @@
-Blockly.Arduino['guan'] = function(block) {
+Blockly.Arduino['tft_espi'] = function(block) {
+  var dropdown___ = block.getFieldValue('種類');
+  var text_miso = block.getFieldValue('miso');
+  var text_mosi = block.getFieldValue('mosi');
+  var text_sclk = block.getFieldValue('sclk');
+  var text_cs = block.getFieldValue('cs');
+  var text_dc = block.getFieldValue('dc');
+  var text_rst = block.getFieldValue('rst');
   // TODO: Assemble Arduino into code variable.
-  var code = 'Serial.println("TEST123");\n';
+  var code='';
+
+  code +='tft.init();\n';
+  code +='tft.setRotation(0);\n';
+  code +='tft.fillScreen(TFT_GREY);\n';
+  code +='tft.setTextColor(TFT_GREEN, TFT_GREY);  // Adding a black background colour erases previous text automatically \n\n';
+  
+  code +='// Draw clock face \n';
+  code +='tft.fillCircle(64, 64, 61, TFT_BLUE);\n';
+  code +='tft.fillCircle(64, 64, 57, TFT_BLACK);\n';
+
+
   return code;
 };
