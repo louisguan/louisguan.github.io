d54e37799f22e47e899dbb85b9b1468be536e910
louisguan <bat869cube672@gmail.com>
2023-03-05 10:38:07 +0800

    Add files via upload

--- BlocklyDuino/ESP32S2/toolbox.js
+++ BlocklyDuino/ESP32S2/toolbox.js
@@ -5,7 +5,7 @@ if (typeof toolbox_custom == 'undefined')
 
 toolbox_custom.push(''
 +'<category id="category_custom" name="官帥積木" colour="100">'
-+'  <category name="TFT全彩顯示">'
++'  <category name="TFT全彩螢幕">'
 +'    <block type="tft_init">'
 +'      <field name="初始化 TFT顯示">初始化 TFT顯示</field>'
 +'      <field name="種類">GC9A01</field>'
